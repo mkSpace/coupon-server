b98d4ee9ac179bad200a92817afde04c68ccb206
Jaemin.Park <once29@naver.com>
2024-03-31 23:55:45 +0900

    Initialize Project

---
+++ src/main/kotlin/com/firstcome/coupon/FirstComeCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.firstcome.coupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstComeCouponApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstComeCouponApplication>(*args)
+}
---
+++ src/test/kotlin/com/firstcome/coupon/FirstComeCouponApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.firstcome.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstComeCouponApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
